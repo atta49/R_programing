84719437f712c1d3a06dc26115cb4d7c196e5832
atta49 <attau49@yahoo.com>
2021-06-05 12:05:40 +0500

    vectorize operation in r programming

---
+++ vectorized_operation.r
@@ -0,0 +1,13 @@
+x<-1:4;y<-5:8
+x+y
+x>2
+x>=2
+y==8
+x/y
+
+#vectorize operation
+rep(1,4)
+x<-matrix(1:4,2,2);y<-matrix(rep(10,4),2,2)
+x*y #element wise multiplication
+x/y
+x%*%y# true multiplication 1st martix row multiply by 2nd martiz column
